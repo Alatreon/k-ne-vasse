ae4f4005cac0a87c90a3874b4cab938ec93153f4
Alateon <dc.shoes@hotmail.fr>
2016-09-05 01:23:35 +0200

    v1

---
+++ js/perso.js
@@ -0,0 +1,55 @@
+function Perso ()
+{
+	this.x = 50;
+	this.y = 50;
+	this.vx= 5;
+	this.vy= 2;
+	// this.color: 'blue';
+}
+Perso.prototype = 
+{
+	draw : function () 
+	{
+	  	ctx.beginPath(); 
+
+		for(var i=0;i<1;i++)
+		{
+			ctx.moveTo(this.x,this.y); 
+			ctx.lineTo(this.x+150,this.y+150);
+			ctx.moveTo(this.x+150,this.y); 
+			ctx.lineTo(this.x,this.y+150);
+		}   
+
+		ctx.closePath();
+
+		ctx.stroke();	
+	},
+	create : function () 
+	{
+		ctx.clearRect(0,0, c.width, c.height);
+
+		this.draw();
+
+		if (this.y + this.vy > c.height || this.y + this.vy < 0)
+		{
+			this.vy = -this.vy;
+		}
+		else
+		{
+			this.x += this.vx;
+		}
+		if (this.x + this.vx > c.width || this.x + this.vx < 0) 
+		{
+			this.vx = -this.vx;
+		}
+		else
+		{
+			this.y += this.vy;			
+		}
+	},
+	anim : function ()
+	{
+		self=this;
+		var test=setInterval(function(){self.create()}, 30)
+	}
+}
--- js/script.js
+++ js/script.js
@@ -1,24 +1,8 @@
 var c = document.getElementById("mon_canvas");
 var ctx = c.getContext("2d");
 
-function createPerso ()
-{
-  	ctx.beginPath(); 
 
-	for(var i=0;i<1;i++)
-	{
-		ctx.moveTo(Math.floor((Math.random() * 350) + 1),Math.floor((Math.random() * 350) + 1)); 
-		ctx.lineTo(Math.floor((Math.random() * 350) + 1),Math.floor((Math.random() * 350) + 1));
-	}   
+var Perso = new Perso;
+Perso.create();
+Perso.anim();
 
-	ctx.closePath();
-
-	ctx.strokeStyle="#"+((1<<24)*Math.random()|0).toString(16);
-	ctx.fill();
-
-	ctx.stroke();
-
-	window.requestAnimationFrame(createPerso);
-}
-
-createPerso();
